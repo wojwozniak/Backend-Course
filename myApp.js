ea4ff0fa9a825b393a8b1bc47f7d74284c17b363
pulchnymamut <pulchnymamut@gmail.com>
2022-06-13 21:24:21 +0200

    Update 1

--- myApp.js
+++ myApp.js
@@ -2,12 +2,11 @@ let express = require('express');
 let app = express();
 
 
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+  });
 
-
-
-
-
-
+  app.use("/public", express.static(__dirname + "/public"));
 
 
 
