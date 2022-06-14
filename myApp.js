5220abbce83e2e1653a0879a312445f3a4239c5b
pulchnymamut <pulchnymamut@gmail.com>
2022-06-14 16:22:36 +0200

    update 2

--- myApp.js
+++ myApp.js
@@ -1,12 +1,25 @@
 let express = require('express');
 let app = express();
+require('dotenv').config();
 
 
-app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/views/index.html");
-  });
+/*app.get("/json", function (req, res) {
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    res.json({ "message": "HELLO JSON" });
+  } else {
+    res.json({ "message": "Hello json" });
+  }
+  }); */
 
-  app.use("/public", express.static(__dirname + "/public"));
+ /* app.use(function middleware(req, res, next) {
+    let string = req.method + " " + req.path + " - " + req.ip;
+    console.log(string);
+    next();
+  }); */
+  
+
+app.use("/public", express.static(__dirname + "/public"));
+
 
 
 
