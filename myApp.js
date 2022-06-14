891e055e007e19f3a0ff203f399e46e5ec601735
pulchnymamut <pulchnymamut@gmail.com>
2022-06-14 21:05:19 +0200

    finished

--- myApp.js
+++ myApp.js
@@ -1,7 +1,14 @@
 let express = require('express');
 let app = express();
 require('dotenv').config();
+let bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({ extended: false })); 
+
+app.post("/name", (req, res) => {
+  let string = req.body.first + " " + req.body.last;
+  res.json({ name: string });
+});
 
 /*app.get("/json", function (req, res) {
   if (process.env.MESSAGE_STYLE === "uppercase") {
@@ -16,7 +23,36 @@ require('dotenv').config();
     console.log(string);
     next();
   }); */
-  
+
+ /* app.get(
+    "/now",
+    (req, res, next) => {
+      req.time = new Date().toString();
+      next();
+    },
+    (req, res) => {
+      res.send({
+        time: req.time
+      });
+    }
+  ); */
+
+/*app.get("/:word/echo", (req, res) => {
+  const { word } = req.params;
+  res.json({
+    echo: word
+  });
+}); */
+
+/*app.get("/name", (req, res) => {
+  let firstName = req.query.first;
+  let lastName = req.query.last;
+  res.json({
+    name: `${firstName} ${lastName}`
+  })
+}); */
+
+app.use()
 
 app.use("/public", express.static(__dirname + "/public"));
 
