7292594b67622f91f349d3f98b5aca5a97df6d9e
pulchnymamut <pulchnymamut@gmail.com>
2022-09-18 17:30:20 +0200

    Repo set up

--- server.js
+++ server.js
@@ -10,9 +10,10 @@ fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.set('view engine','pug');
 
 app.route('/').get((req, res) => {
-
+  res.render(process.cwd()+'/views/pug/index.pug');
 });
 
 const PORT = process.env.PORT || 3000;
